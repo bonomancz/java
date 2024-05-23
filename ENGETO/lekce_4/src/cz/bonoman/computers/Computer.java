8b6995f78454d589549105367cfaca4f42434863
bonoman@volny.cz <bonoman@volny.cz>
2024-05-23 19:55:25 +0200

    added Computer class

---
+++ ENGETO/lekce_4/src/cz/bonoman/computers/Computer.java
@@ -0,0 +1,10 @@
+package cz.bonoman.computers;
+
+import java.time.LocalDate;
+
+public class Computer {
+    private String brand, model, description;
+    private int year;
+    private double weight;
+    private LocalDate purchaseDate;
+}
