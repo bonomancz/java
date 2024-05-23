354bc50f473ee655cf2d8dc481d7e933ff07259c
bonoman@volny.cz <bonoman@volny.cz>
2024-05-23 20:02:00 +0200

    added getters - Computer class

--- ENGETO/lekce_4/src/cz/bonoman/computers/Computer.java
+++ ENGETO/lekce_4/src/cz/bonoman/computers/Computer.java
@@ -7,4 +7,17 @@ public class Computer {
     private int year;
     private double weight;
     private LocalDate purchaseDate;
+
+    public Computer(String description, int year, double weight, LocalDate purchaseDate){
+        this.description = description;
+        this.year = year;
+        this.weight = weight;
+        this.purchaseDate = purchaseDate;
+    }
+
+    public double getWeight() {return weight;}
+    public int getYear() {return year;}
+    public String getBrand() {return brand;}
+    public String getDescription() {return description;}
+    public LocalDate getPurchaseDate() {return purchaseDate;}
 }
