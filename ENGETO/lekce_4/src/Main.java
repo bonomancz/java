1a09182db809e17e0502b0a28463975eceb9d3b3
bonoman@volny.cz <bonoman@volny.cz>
2024-05-23 20:33:13 +0200

    defined computers

--- ENGETO/lekce_4/src/Main.java
+++ ENGETO/lekce_4/src/Main.java
@@ -1,5 +1,27 @@
+import cz.bonoman.computers.Computer;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Main {
+
     public static void main(String[] args){
-        System.out.println("Working with github repository 4th lessons.");
+        System.out.println("Computer class in 4th lesson.");
+        ArrayList<Computer> computers = new ArrayList<>();
+
+        // define computers
+        computers.add(new Computer("IBM", "Recepce",2015, 21, LocalDate.of(2024, 5, 11)));
+        computers.add(new Computer("Apple", "Reditel", 2018, 21, LocalDate.of(2017, 10, 6)));
+        computers.add(new Computer("Dell", "Office1", 2014, 21, LocalDate.of(2013, 12, 23)));
+        computers.add(new Computer("Autocont", "Office2", 2009, 21, LocalDate.of(2007, 6, 10)));
+        computers.add(new Computer("HP", "Office3", 2001, 21, LocalDate.of(2001, 7, 5)));
+
+
+
+        // output
+        for(Computer computer : computers) {
+            System.out.println(computer.getDescription() + " " + computer.getBrand() + " " + computer.getYear());
+        }
     }
+
 }
--- ENGETO/lekce_4/src/cz/bonoman/computers/Computer.java
+++ ENGETO/lekce_4/src/cz/bonoman/computers/Computer.java
@@ -2,17 +2,18 @@ package cz.bonoman.computers;
 
 import java.time.LocalDate;
 
-public class Computer {
+public class Computer implements Comparable<Computer> {
     private String brand, model, description;
     private int year;
     private double weight;
     private LocalDate purchaseDate;
 
-    public Computer(String description, int year, double weight, LocalDate purchaseDate){
+    public Computer(String brand, String description, int year, double weight, LocalDate purchaseDate){
         this.description = description;
         this.year = year;
         this.weight = weight;
         this.purchaseDate = purchaseDate;
+        this.brand = brand;
     }
 
     public double getWeight() {return weight;}
@@ -20,4 +21,9 @@ public class Computer {
     public String getBrand() {return brand;}
     public String getDescription() {return description;}
     public LocalDate getPurchaseDate() {return purchaseDate;}
+
+    @Override
+    public int compareTo(Computer o) {
+        return 0;
+    }
 }
